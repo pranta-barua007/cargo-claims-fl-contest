c7e5ac113b37613628e9ebd8a10deb17b127b054
pranta-barua007 <prantabarua247@gmail.com>
2021-04-13 22:44:07 +0600

    added about&contact section update hero

---
+++ assets/scripts/owl.main.js
@@ -0,0 +1,34 @@
+jQuery("#carousel").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    rewind: true,
+    margin: 10,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 4
+      },
+  
+      1366: {
+        items: 6
+      }
+    }
+  });
+
+ 
